7d33cc20b883b70cfb4b078241440af9b1e628fc
PedroReoli <pedrolucas2012@gmail.com>
2025-10-23 12:03:07 -0300

    Fixing errors

--- site/src/components/atoms/Container.tsx
+++ site/src/components/atoms/Container.tsx
@@ -27,29 +27,3 @@ export const Container: React.FC<ContainerProps> = ({
 }
 
 
-interface ContainerProps {
-  children: React.ReactNode
-  className?: string
-  maxWidth?: 'sm' | 'md' | 'lg' | 'xl' | 'full'
-}
-
-export const Container: React.FC<ContainerProps> = ({
-  children,
-  className = '',
-  maxWidth = 'xl'
-}) => {
-  const maxWidthStyles = {
-    sm: 'max-w-3xl',
-    md: 'max-w-5xl',
-    lg: 'max-w-6xl',
-    xl: 'max-w-7xl',
-    full: 'max-w-full'
-  }
-  
-  return (
-    <div className={`${maxWidthStyles[maxWidth]} mx-auto px-6 ${className}`}>
-      {children}
-    </div>
-  )
-}
-
--- site/src/components/templates/LandingPageTemplate.tsx
+++ site/src/components/templates/LandingPageTemplate.tsx
@@ -25,29 +25,3 @@ export const LandingPageTemplate: React.FC = () => {
   )
 }
 
-import { HeroSection } from '../organisms/HeroSection'
-import { AboutSection } from '../organisms/AboutSection'
-import { EcosystemSection } from '../organisms/EcosystemSection'
-import { ChallengesSection } from '../organisms/ChallengesSection'
-import { MethodologySection } from '../organisms/MethodologySection'
-import { AchievementsSection } from '../organisms/AchievementsSection'
-import { ImpactSection } from '../organisms/ImpactSection'
-import { FutureSection } from '../organisms/FutureSection'
-import { FooterSection } from '../organisms/FooterSection'
-
-export const LandingPageTemplate: React.FC = () => {
-  return (
-    <main className="min-h-screen">
-      <HeroSection />
-      <AboutSection />
-      <EcosystemSection />
-      <ChallengesSection />
-      <MethodologySection />
-      <AchievementsSection />
-      <ImpactSection />
-      <FutureSection />
-      <FooterSection />
-    </main>
-  )
-}
-
